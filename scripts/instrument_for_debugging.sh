5239adcb5078d2f3abe868df195d4dad38e3752f
Michael Pradel <michael@binaervarianz.de>
2014-05-22 15:51:20 -0700

    simplified debugging scripts

--- scripts/instrument_for_debugging.sh
+++ scripts/instrument_for_debugging.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-node ../jalangi/src/js/commands/instrument.js --copy_runtime --smemory --inbrowser --analysis src/js/analyses/CommonUtil.js --analysis src/js/analyses/inconsistentType/CallGraph.js --analysis src/js/analyses/inconsistentType/TypeAnalysis.js --analysis src/js/analyses/inconsistentType/InconsistentTypeEngine.js --outputDir instrumented/ tests/inconsistentType/
+rm -rf instrument_for_debugging_tmp
+mkdir instrument_for_debugging_tmp
+cp $1 instrument_for_debugging_tmp/file.js
+
+node src/js/analyses/inconsistentType/Preprocessor.js instrument_for_debugging_tmp/file.js instrument_for_debugging_tmp/file_beliefs_.js
+
+cp scripts/debugging_template.html instrument_for_debugging_tmp/index.html
+
+node ../jalangi/src/js/commands/instrument.js --direct_in_output --copy_runtime --smemory --inbrowser --analysis src/js/analyses/CommonUtil.js --analysis src/js/analyses/inconsistentType/CallGraph.js --analysis src/js/analyses/inconsistentType/TypeAnalysis.js --analysis src/js/analyses/inconsistentType/InconsistentTypeEngine.js --outputDir instrumented/ instrument_for_debugging_tmp
+
+rm -rf instrument_for_debugging_tmp
+
