4ded67f8f9a938d0202569c6b3e980564d9176f8
Michael Pradel <michael@binaervarianz.de>
2014-12-13 14:32:49 +0100

    update of Selenium version

--- scripts/type_coercions_websites.sh
+++ scripts/type_coercions_websites.sh
@@ -3,15 +3,15 @@
 dir="type_coercions_results/websites"
 
 rm -rf instrumentFF_tmp
-rm -rf ${dir}
 mkdir type_coercions_results
 mkdir ${dir}
 for bm in `cat tests/typeCoercion/urls.txt | xargs`
 do
   echo "####################################"
   echo ${bm}
-  java -cp thirdparty/selenium-server-standalone-2.41.0.jar:/home/m/eclipse/workspace/WebAppEvaluation/bin/ evaluation.ExperimentRunner --url ${bm}
+  java -cp thirdparty/selenium-server-standalone-2.44.0.jar:/home/m/eclipse/workspace/WebAppEvaluation/bin/ evaluation.ExperimentRunner --url ${bm}
   bm_short=`basename ${bm}`
+  rm -rf ${dir}/${bm_short}
   mkdir ${dir}/${bm_short}
   mv /tmp/analysisResults.json ${dir}/${bm_short}/analysisResults.json
   mkdir ${dir}/${bm_short}/sourcemaps
@@ -19,4 +19,3 @@ do
   mkdir ${dir}/${bm_short}/src
   mv instrumentFF_tmp/*.js ${dir}/${bm_short}/src/
 done
-
