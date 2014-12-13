b39b073fae013b359a32689349b051ce348b25bf
Michael Pradel <michael@binaervarianz.de>
2014-12-13 14:32:32 +0100

    another test

---
+++ tests/html/unitApps/app_type_coercions/mini.js
@@ -0,0 +1,25 @@
+(function() {
+
+    // potentially harmful coercions
+    if (!(new Boolean(false))) {
+        var x;
+        x = x + "abc";
+        var y = {} >> [4,3,2,1];
+    }
+
+    // harmless coercions
+    if (23) {
+        "aa" + true;
+        5 - new Number(20);
+    }
+
+
+    function foo() {}
+
+    foo();
+    foo();
+
+
+    console.log("done");
+
+})();
